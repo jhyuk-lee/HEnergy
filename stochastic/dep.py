d8cc811e6276a6a4f3c5522effd1f81272128ea5
swkim <lomanope@gmail.com>
2024-12-17 19:08:57 +0900

    scenred 폴더 가져옴. 시나리오 생성 코드

--- stochastic/dep.py
+++ stochastic/dep.py
@@ -1,4 +1,8 @@
-﻿import pyomo.environ as pyo
+﻿import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # 부모의 부모 디렉토리 추가
+import scenred.scenario_reduction
+import pyomo.environ as pyo
 import numpy as np
 import pandas as pd
 # import pyro
@@ -7,7 +11,7 @@ import pandas as pd
 # from pyro.optim import Adam
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
-import os
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.mixture import GaussianMixture
@@ -27,6 +31,217 @@ from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 import preproc
 
-preproc.generate_scenarios()
+# preproc.generate_scenarios()
+# 최적화 모델 생성
 
+def build_model(scenarios, E_0):
+    model = pyo.ConcreteModel()
+
+    # Parameters
+    model.E_0 = pyo.Param(initialize=E_0, mutable=True)
+
+    num_scenarios = len(scenarios)
+
+    # First-stage decision variables
+    model.b_da = pyo.Var(bounds=(-P_r, 0), domain=pyo.Reals, initialize = -P_r / 2)
+    model.q_da = pyo.Var(domain=pyo.Reals, bounds=(0.9*E_0, 1.1*E_0), initialize = E_0)
+
+    # Scenario-specific components
+    model.scenarios = pyo.RangeSet(0, num_scenarios - 1)
+    model.P_da = pyo.Param(model.scenarios, initialize={i: scenarios[i][0] for i in range(num_scenarios)})
+    model.P_rt = pyo.Param(model.scenarios, initialize={i: scenarios[i][1] for i in range(num_scenarios)})
+    model.E_1 = pyo.Param(model.scenarios, initialize={i: scenarios[i][2] for i in range(num_scenarios)})
+    model.random_factor = pyo.Param(model.scenarios, initialize={i: scenarios[i][3] for i in range(num_scenarios)})
+
+    # Second-stage decision variables
+    def q_rt_bounds(model, s):
+        return model.E_1[s]
+    model.b_rt = pyo.Var(model.scenarios, bounds=(-P_r, 0), domain=pyo.Reals, initialize = - P_r / 2)
+    model.q_rt = pyo.Var(model.scenarios, bounds=q_rt_bounds, domain=pyo.Reals, initialize=lambda model, s: model.E_1[s])
+
+    # Scenario-specific binary variables
+    model.y_da = pyo.Var(model.scenarios, domain=pyo.Binary)
+    model.y_rt = pyo.Var(model.scenarios, domain=pyo.Binary)
+
+    # Scenario-specific analysis variables
+    model.Q_da = pyo.Var(model.scenarios, domain=pyo.NonNegativeReals, initialize= model.q_da / 2)
+    model.Q_rt = pyo.Var(model.scenarios, domain=pyo.NonNegativeReals, initialize=lambda model, s: model.q_rt[s] / 2)
+    model.Q_c = pyo.Var(model.scenarios, domain=pyo.NonNegativeReals, initialize=lambda model, s: model.random_factor[s])
+
+    # Third-stage decision variables
+    def z_bounds(model, s):
+        return (0, model.E_1[s])
+    model.z = pyo.Var(model.scenarios, bounds=z_bounds, domain=pyo.Reals, initialize=lambda model, s: model.E_1[s])
+
+    # Linearization Real Variables
+    model.m1_V = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.m2_V = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.m1_E = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.m2_E = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.m3_E = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.m1_Im = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.m2_Im = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.m3_Im = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.m4_Im = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.S1_V = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.S1_E = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.S1_Im = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.S2_Im = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.f_E = pyo.Var(model.scenarios, domain=pyo.Reals)
+    model.f_max = pyo.Var(model.scenarios, domain=pyo.Reals)
+
+    # Linearization Binary Variables (Scenario-specific)
+    model.n1_V = pyo.Var(model.scenarios, domain=pyo.Binary)
+    model.n2_V = pyo.Var(model.scenarios, domain=pyo.Binary)
+    model.n1_E = pyo.Var(model.scenarios, domain=pyo.Binary)
+    model.n2_E = pyo.Var(model.scenarios, domain=pyo.Binary)
+    model.n3_E = pyo.Var(model.scenarios, domain=pyo.Binary)
+    model.n1_Im = pyo.Var(model.scenarios, domain=pyo.Binary)
+    model.n2_Im = pyo.Var(model.scenarios, domain=pyo.Binary)
+    model.n3_Im = pyo.Var(model.scenarios, domain=pyo.Binary)
+    model.n4_Im = pyo.Var(model.scenarios, domain=pyo.Binary)
+    model.n1_F = pyo.Var(model.scenarios, domain=pyo.Binary)
+    model.n2_F = pyo.Var(model.scenarios, domain=pyo.Binary)
+    model.n3_F = pyo.Var(model.scenarios, domain=pyo.Binary)
+
+    # Constraints
+    model.constrs = pyo.ConstraintList() ## SW: constrs list로 만들어서 넣으면 엄청 느린데..
+    def scenario_constraints(model, s):
+
+        model.constrs.add(model.b_da - model.P_da[s] <= M * (1 - model.y_da[s]))
+        model.constrs.add(model.q_da <= M * model.y_da[s])
+        model.constrs.add(model.Q_da[s] <= model.q_da)
+        model.constrs.add(model.q_da - M * (1 - model.y_da[s]) <= model.Q_da[s])
+
+        model.constrs.add(model.b_rt[s] - model.P_rt[s] <= M * (1 - model.y_rt[s]))
+        model.constrs.add(model.q_rt[s] <= M * model.y_rt[s])
+        model.constrs.add(model.q_rt[s] - M * (1 - model.y_rt[s]) <= model.Q_rt[s])
+        model.constrs.add(model.Q_rt[s] <= model.q_rt[s])
+ 
+        # Rule constraint
+        model.constrs.add(model.Q_c[s] == model.random_factor[s] * model.Q_rt[s])
+        model.constrs.add(model.b_rt[s] <= model.b_da)
+
+        # f_V Linearization constraints
+        model.constrs.add(model.S1_V[s] == model.b_rt[s] * model.m1_V[s] - model.Q_da[s] * model.P_da[s] - model.m1_V[s] * model.P_rt[s] + model.P_rt[s] * model.Q_da[s])
+    
+        model.constrs.add(model.m1_V[s] <= model.z[s])
+        model.constrs.add(model.m1_V[s] <= model.Q_c[s])
+        model.constrs.add(model.m1_V[s] >= model.z[s] - M * (1 - model.n1_V[s]))
+        model.constrs.add(model.m1_V[s] >= model.Q_c[s] - M * model.n1_V[s])
+
+        model.constrs.add(model.m2_V[s] >= model.S1_V[s])
+        model.constrs.add(model.m2_V[s] >= 0)
+        model.constrs.add(model.m2_V[s] <= model.S1_V[s] + M * (1 - model.n2_V[s]))
+        model.constrs.add(model.m2_V[s] <= M * model.n2_V[s])
+
+        # f_E Linearization constraints
+        model.constrs.add(model.m1_E[s] <= model.Q_da[s])
+        model.constrs.add(model.m1_E[s] <= model.q_rt[s])
+        model.constrs.add(model.m1_E[s] >= model.Q_da[s] - M * (1 - model.n1_E[s]))
+        model.constrs.add(model.m1_E[s] >= model.q_rt[s] - M * model.n1_E[s])
+  
+        model.constrs.add(model.m2_E[s] <= model.z[s])
+        model.constrs.add(model.m2_E[s] <= model.q_rt[s])
+        model.constrs.add(model.m2_E[s] >= model.z[s] - M * (1 - model.n2_E[s]))
+        model.constrs.add(model.m2_E[s] >= model.q_rt[s] - M * model.n2_E[s])
+  
+        model.constrs.add(model.f_E[s] >= (model.P_rt[s] - model.b_da) * model.m1_E[s] - M * (1 - model.n3_E[s]))
+        model.constrs.add(model.f_E[s] <= (model.P_rt[s] - model.b_da) * model.m2_E[s] + M * model.n3_E[s])
+        model.constrs.add(model.f_E[s] == (model.P_rt[s] - model.b_da) * (model.m1_E[s] - model.m2_E[s]))
+        
+        # f_Im Linearization constraints
+        model.constrs.add(model.S1_Im[s] == (model.z[s] - model.Q_c[s]) - 0.12 * C)
+
+        model.constrs.add(model.S2_Im[s] == model.P_rt[s] - model.b_rt[s])
+
+        model.constrs.add(model.m1_Im[s] >= model.S1_Im[s])
+        model.constrs.add(model.m1_Im[s] >= 0)
+        model.constrs.add(model.m1_Im[s] <= model.S1_Im[s] + M * (1 - model.n1_Im[s]))
+        model.constrs.add(model.m1_Im[s] <= M * model.n1_Im[s])
+
+        model.constrs.add(model.m2_Im[s] >= model.S2_Im[s])
+        model.constrs.add(model.m2_Im[s] >= 0)
+        model.constrs.add(model.m2_Im[s] <= model.S2_Im[s] + M * (1 - model.n2_Im[s]))
+        model.constrs.add(model.m2_Im[s] <= M * model.n2_Im[s])
+        model.constrs.add(model.m3_Im[s] >= -model.b_rt[s])
+        model.constrs.add(model.m3_Im[s] >= 0)
+        model.constrs.add(model.m3_Im[s] <= -model.b_rt[s] + M * (1 - model.n3_Im[s]))
+        model.constrs.add(model.m3_Im[s] <= M * model.n3_Im[s])
+        model.constrs.add(model.m4_Im[s] >= model.m3_Im[s])
+        model.constrs.add(model.m4_Im[s] >= model.m2_Im[s])
+        model.constrs.add(model.m4_Im[s] <= model.m3_Im[s] + M * (1 - model.n4_Im[s]))
+        model.constrs.add(model.m4_Im[s] <= model.m2_Im[s] + M * model.n4_Im[s])
+
+        # f_max linearization constraints
+        model.constrs.add(model.f_max[s] >= model.m2_V[s])
+        model.constrs.add(model.f_max[s] >= model.f_E[s])
+        model.constrs.add(model.f_max[s] >= 0)
+        model.constrs.add(model.f_max[s] <= model.m2_V[s] + M * (1 - model.n1_F[s]))
+        model.constrs.add(model.f_max[s] <= model.f_E[s] + M * (1 - model.n2_F[s]))
+        model.constrs.add(model.f_max[s] <= M * (1 - model.n3_F[s]))
+        model.constrs.add(model.n1_F[s] + model.n2_F[s] + model.n3_F[s] == 1)
+        
+    for s in model.scenarios:
+        scenario_constraints(model, s)
+
+    nonanti_scen_list = []
+    # Non-anticipativity constraints
+    for i in range(num_scenarios):
+        for j in range(i + 1, num_scenarios):
+            if scenarios[i][0] == scenarios[j][0] and scenarios[i][2] == scenarios[j][2]:
+                model.constrs.add(model.b_rt[i] == model.b_rt[j])
+                model.constrs.add(model.q_rt[i] == model.q_rt[j])  
+                nonanti_scen_list.append(i)
+    model.nonanti_scen = pyo.Set(initialize=nonanti_scen_list) #model에 저장
+    # Objective Function
+    def objective_rule(model):
+        return sum((model.P_da[s] * model.Q_da[s] + model.P_rt[s] * (model.z[s] - model.Q_da[s])) + model.f_max[s] + (-model.m1_Im[s] * model.m4_Im[s]) + (model.z[s] * P_r) for s in model.scenarios)/len(model.scenarios)
+
+    model.objective = pyo.Objective(rule=objective_rule, sense=pyo.maximize)
+
+    return model
+def calculate_profit(model, idx):
+    return (
+        pyo.value(model.P_da[idx]) * pyo.value(model.Q_da[idx]) +
+        pyo.value(model.P_rt[idx]) * (pyo.value(model.z[idx]) - pyo.value(model.Q_da[idx])) +
+        pyo.value(model.f_max[idx]) - (pyo.value(model.m1_Im[idx]) * pyo.value(model.m4_Im[idx])) +
+        pyo.value(model.z[idx]) * P_r
+    )
+def solve_sp_model(scenarios, new_E_0):
+    model = build_model(scenarios, new_E_0)
+    model.E_0 = new_E_0
+    solver = pyo.SolverFactory('gurobi')
+    solver.options['NonConvex'] = 2
+    solver.solve(model, tee=True)
+    print('insample profit: ', pyo.value(model.objective))
+    print('b_da: ', pyo.value(model.b_da))
+    print('q_da: ', pyo.value(model.q_da))
+    results = []
+    for s in range(len(scenarios)):
+        profit = calculate_profit(model, s)
+        results.append({
+            # "b_da": pyo.value(model.b_da),
+            # "q_da": pyo.value(model.q_da),
+            "b_rt": pyo.value(model.b_rt[s]),
+            "q_rt": pyo.value(model.q_rt[s]),
+            "z": pyo.value(model.z[s]),
+            "Profit": profit
+        })
+
+    return model, pd.DataFrame(results)
+
+P_r = 80
+C = 18330.6
+M = 10**6
+
+new_E_0_values = [10000, 8000, 12000]
+
+scenarios = preproc.regenerate_scenarios(new_E_0_values[1], sample_size=10)
+for scen in scenarios:
+    print(scen)
+model, res = solve_sp_model(scenarios, new_E_0_values[1])
+
+scenarios[1]
+print(scenarios)
 
--- stochastic/preproc.py
+++ stochastic/preproc.py
@@ -41,8 +41,8 @@ def process_rt_file(file_path):
 
 # 가격 시나리오 생성 함수
 def generate_price_scenarios(filtered_df, N_price, seed=None):
-    if seed:
-        np.random.seed(seed)
+    # if seed:
+    # np.random.RandomState(seed)
     
     P_da_distribution = filtered_df['P(da)'].tolist()
     P_rt_distribution = filtered_df['P(rt)'].tolist()
@@ -65,6 +65,7 @@ def generate_price_scenarios(filtered_df, N_price, seed=None):
 
 # 발전량 시나리오 생성 함수
 def generate_E_1_scenarios(N_E1, kde, E_0, E_0_values, E_1_values):
+    # np.random.RandomState(seed) # seed 고정
     E_0_min, E_0_max = E_0_values.min(), E_0_values.max()
     E_0_range = E_0_max - E_0_min 
 
@@ -84,7 +85,7 @@ def generate_E_1_scenarios(N_E1, kde, E_0, E_0_values, E_1_values):
 
 # 급전지시량 시나리오 생성 함수
 def generate_random_factor_scenarios(N_Qc=10, mean=1.0, std_dev=0.2):
-
+    # np.random.RandomState(seed)
     num_ones = int(0.9 * N_Qc)
     ones = [1.0] * num_ones
 
@@ -138,7 +139,35 @@ def generate_scenarios_with_streaming(filtered_df, N_E1, N_price, kde, E_0, E_0_
 
     return reservoir
 
-def generate_scenarios():
+def regenerate_scenarios_with_streaming(filtered_df, kde, new_E_0, E_0_values, E_1_values, N_price, N_E1, sample_size):
+    P_da_samples, P_rt_samples = generate_price_scenarios(filtered_df, N_price)
+
+    E_1_scenarios = generate_E_1_scenarios(N_E1, kde, new_E_0, E_0_values, E_1_values)
+
+    random_factors = generate_random_factor_scenarios()
+
+    # da_indices = np.random.randint(0, N_price, sample_size)
+    # rt_indices = np.random.randint(0, N_price, sample_size)
+    # e1_indices = np.random.randint(0, N_E1, sample_size)
+    # all_scenarios = itertools.product(P_da_samples, P_rt_samples, E_1_scenarios, random_factors)
+
+    reservoir = []
+
+    reservoir = [(P_da_samples[i], P_rt_samples[i],
+                  E_1_scenarios[i], random_factors[i]) for i in range(sample_size)]
+    
+    # for idx, scenario in enumerate(all_scenarios):
+    #     if len(reservoir) < sample_size:
+    #         reservoir.append(scenario)
+    #     else:
+    #         # replace_idx = random.randint(0, idx)
+    #         # if replace_idx < sample_size:
+    #         #     reservoir[replace_idx] = scenario
+    #         pass
+
+    return reservoir
+
+def generate_all_scenarios():
     curr_dir = Path(__file__).parent
     project_dir = curr_dir.parent
     target_dir = project_dir / '모의 실시간시장 가격'
@@ -431,8 +460,8 @@ def generate_scenarios():
     # print()
 
     target_dir = curr_dir / 'mc_scenarios'
-    mc_path = os.path.join(target_dir, 'mc_scenarios')
-    all_path = os.path.join(target_dir, 'all_scenarios')
+    mc_path = os.path.join(target_dir, 'mc_scenarios_E_0_'+str(E_0))
+    all_path = os.path.join(target_dir, 'all_scenarios'+str(E_0))
 
     with open('{}.npy'.format(mc_path), 'wb') as f:
         np.save(f, mc_scenarios)
@@ -440,6 +469,158 @@ def generate_scenarios():
         np.save(f, all_scenarios)
 
 
+    # print("Sampled MC Scenarios (Top 10):")
+    # for i, scenario in enumerate(mc_scenarios[:10], 1):
+    #     print(f"MC Scenario {i}:  P_da = {scenario[0]:.2f}, P_rt = {scenario[1]:.2f}, E_1 = {scenario[2]:.2f}, Random Factor = {scenario[3]:.2f}")
+
+
+
+def regenerate_scenarios(new_E_0, sample_size):
+    curr_dir = Path(__file__).parent
+    project_dir = curr_dir.parent
+    target_dir = project_dir / '모의 실시간시장 가격'
+    directory_path_da = str(target_dir / '하루전')
+    directory_path_rt = str(target_dir / '실시간 확정')
+
+
+    # print(f"Current directory: {curr_dir}")
+    # print(f"Project directory: {project_dir}")
+    # print(f"Target directory: {target_dir}")
+
+
+
+    csv_files_da = [f for f in os.listdir(directory_path_da) if f.endswith('.csv')]
+    csv_files_rt = [f for f in os.listdir(directory_path_rt) if f.endswith('.csv')]
+
+    data = []
+
+    for da_file, rt_file in zip(csv_files_da, csv_files_rt):
+        match = re.search(r'(\d{8})', da_file)
+        if match:
+            date_str = match.group(1)
+            date = datetime.strptime(date_str, '%Y%m%d')
+
+            day_ahead_data = process_da_file(os.path.join(directory_path_da, da_file))
+            real_time_data = process_rt_file(os.path.join(directory_path_rt, rt_file))
+
+            for hour in range(24):
+                timestamp = date + timedelta(hours=hour)
+                data.append({'P(da)': day_ahead_data[hour], 'P(rt)': real_time_data[hour], 'timestamp': timestamp})
+                # print(data)
+
+    original_price_df = pd.DataFrame(data)
+    # global original_price_df
+    # print(original_price_df)
+
+
+    E_1_df = pd.read_csv(str(curr_dir) + '\\jeju_estim.csv')
+    E_1_df['timestamp'] = pd.to_datetime(E_1_df['timestamp'])
+    E_1_df['timestamp'] = E_1_df['timestamp'].dt.tz_localize(None)
+    E_1_df.rename(columns={'forecast_da': 'E_0', 'forecast_rt': 'E_1', 'timestamp': 'date'}, inplace=True)
+
+    E_1_df = (E_1_df.set_index('date').resample('H').sum().reset_index())
+
+    # E_1_df
+
+
+    # 시간대 6시 - 18시, 날짜 3월 ~ 현재
+
+    E_1_filtered = E_1_df.loc[(E_1_df['date'].dt.hour >= 6) & (E_1_df['date'].dt.hour <= 18)].copy()
+    P_da_filtered = original_price_df.loc[(original_price_df['timestamp'].dt.hour >= 6) & 
+                                        (original_price_df['timestamp'].dt.hour <= 18) & 
+                                        (original_price_df['timestamp'].dt.minute == 0)].copy()
+    P_rt_filtered = original_price_df.loc[(original_price_df['timestamp'].dt.hour >= 6) & 
+                                        (original_price_df['timestamp'].dt.hour <= 18)].copy()
+
+    E_1_filtered['date'] = pd.to_datetime(E_1_filtered['date'])
+    E_1_filtered['hour'] = E_1_filtered['date'].dt.hour
+
+    P_da_filtered['date'] = pd.to_datetime(P_da_filtered['timestamp'])
+    P_da_filtered['hour'] = P_da_filtered['timestamp'].dt.hour
+
+    P_rt_filtered['date'] = pd.to_datetime(P_rt_filtered['timestamp'])
+    P_rt_filtered['hour'] = P_rt_filtered['timestamp'].dt.hour
+
+    start_date = '2024-03-01'
+    end_date = '2024-11-25'
+
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    E_1_filtered = E_1_filtered[(E_1_filtered['date'] >= start_date) & (E_1_filtered['date'] <= end_date)]
+    P_da_filtered = P_da_filtered[(P_da_filtered['date'] >= start_date) & (P_da_filtered['date'] <= end_date)]
+    P_rt_filtered = P_rt_filtered[(P_rt_filtered['date'] >= start_date) & (P_rt_filtered['date'] <= end_date)]
+
+    merged_df = pd.merge(E_1_filtered, P_da_filtered[['date', 'hour', 'P(da)']], on=['date', 'hour'], how='left')
+    merged_df = pd.merge(merged_df, P_rt_filtered[['date', 'hour', 'P(rt)']], on=['date', 'hour'], how='left')
+    merged_df.fillna(0, inplace=True)
+
+    merged_df['date'] = merged_df.apply(lambda row: row['date'].replace(hour=row['hour']), axis=1)
+    merged_df.drop(columns=['hour'], inplace=True)
+
+    # merged_df
+ 
+    filtered_df = merged_df[(merged_df['P(da)'] < 0) & 
+                            (merged_df['P(rt)'] < 0) & 
+                            (merged_df['P(da)'] > merged_df['P(rt)'])]
+
+    filtered_df.reset_index(drop=True, inplace=True)
+    filtered_df.index = filtered_df.index + 1
+
+    # filtered_df
+
+
+
+
+    # E_0 기반 E_1 conditional KDE
+    E_0_values = merged_df['E_0'].values.reshape(-1, 1)
+    E_1_values = merged_df['E_1'].values
+
+    kde = KernelDensity(kernel='gaussian', bandwidth=10)
+    kde.fit(np.hstack([E_0_values, E_1_values.reshape(-1, 1)]))
+
+    E_0_range = np.linspace(E_0_values.min(), E_0_values.max(), 100)
+    E_1_range = np.linspace(E_1_values.min(), E_1_values.max(), 100)
+    E_0_grid, E_1_grid = np.meshgrid(E_0_range, E_1_range)
+    grid_points = np.vstack([E_0_grid.ravel(), E_1_grid.ravel()]).T
+
+    log_density = kde.score_samples(grid_points)
+    density = np.exp(log_density).reshape(E_0_grid.shape)
+
+
+    print("sampling 시작")
+    start_time = time.time()
+
+    # 전체 시나리오 생성
+    global seed
+    seed = 42
+    np.random.seed(seed)
+    N_E1 = 200
+    N_price = 200
+    
+    total_sample_size = sample_size
+    E_0 = new_E_0
+
+    regen_scenarios = regenerate_scenarios_with_streaming(filtered_df, kde, new_E_0=E_0, E_0_values=E_0_values, E_1_values=E_1_values,
+                                                           N_price=200, N_E1=200, sample_size=total_sample_size)
+    # all_scenarios = generate_scenarios_with_streaming(filtered_df, N_E1, N_price, kde, E_0, E_0_values, E_1_values, sample_size=total_sample_size)
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+
+    # 실행 시간 출력
+    print(f"Total Execution Time: {execution_time:.2f} seconds")
+    # print()
+
+    target_dir = curr_dir / 'mc_scenarios'
+    mc_path = os.path.join(target_dir, 'mc_scenarios')
+    all_path = os.path.join(target_dir, 'all_scenarios')
+
+    # with open('{}.npy'.format(mc_path), 'wb') as f:
+    #     np.save(f, mc_scenarios)
+    # with open('{}.npy'.format(all_path), 'wb') as f:
+    #     np.save(f, all_scenarios)
+
+    return regen_scenarios
     # print("Sampled MC Scenarios (Top 10):")
     # for i, scenario in enumerate(mc_scenarios[:10], 1):
     #     print(f"MC Scenario {i}:  P_da = {scenario[0]:.2f}, P_rt = {scenario[1]:.2f}, E_1 = {scenario[2]:.2f}, Random Factor = {scenario[3]:.2f}")
